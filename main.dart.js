a198b439b04ffe436018f1bf4e84b23b4c74faa8
seasu <seasu@users.noreply.github.com>
2026-03-24 14:51:18 +0000

    deploy: 4106decaf10866c1c1fc92f0c716bc8383ff831d

--- main.dart.js
+++ main.dart.js
@@ -29898,7 +29898,7 @@ $S:2}
 A.aB9.prototype={
 $1(a){var s=A.dB().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/052f31d115eceda8cbff1b3481fcde4330c4ae12/":s)+a},
 $S:54}
 A.OR.prototype={
 gB(a){var s=this.a
